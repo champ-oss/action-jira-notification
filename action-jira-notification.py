fac396cbc441795a59509fea6f9a619e51c4d6c2
Phillip Langley <philliplangley@champmbplangley.lan>
2024-04-15 16:31:20 -0500

    resolve linting issue

--- action-jira-notification.py
+++ action-jira-notification.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # usage: python action-jira-notification.py
-# export JIRA_TOKEN, JIRA_HOST, JIRA_PROJECT, JIRA_USER, GITHUB_WORKFLOW_NAME and GIT_REPO as env variable
+# export JIRA_TOKEN, JIRA_HOST, JIRA_PROJECT, JIRA_USER, JIRA_TYPE as env variable
 #########################################################################################################
 # coding=utf-8
 from atlassian import Jira

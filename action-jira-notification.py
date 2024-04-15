541fd0ffa51ee60b2bf1fbd719edf0fbb3616a64
Phillip Langley <philliplangley@champmbplangley.lan>
2024-04-15 16:09:15 -0500

    updated jira action

--- action-jira-notification.py
+++ action-jira-notification.py
@@ -19,51 +19,54 @@ def jira_auth(host: str, user: str, token: str) -> any:
 
 def check_existing_issue(jira: str, project: str, repo: str, workflow_name: str) -> any:
     # default jira JQL query string
-    JQL = "project = " + project + " AND labels = " + repo + " AND labels = " + workflow_name + " AND (status = 'To Do' OR status = 'In Progress')"
+    jql = ("project = " + project + " AND labels = " + repo + " AND labels = " + workflow_name +
+           " AND (status != 'Done' AND status != 'Resolved')")
 
-    data = jira.jql(JQL)
+    data = jira.jql(jql)
     # checking json total value
     get_total_issue_count = data["total"]
 
     return get_total_issue_count
 
 
-def create_jira_issue(jira: str, project: str, repo: str, workflow_name: str, jira_type: str) -> None:
-    jira.issue_create(
-        fields={
-            "project": {
-                "key": project
-            },
-            "labels": [
-                repo,
-                workflow_name
-            ],
-            "issuetype": {
-                "name": jira_type
-            },
-            "summary": "" + repo + " failure",
-            "description": "" + repo + " - " + workflow_name + " failure",
-        }
+def get_description(repo: str, workflow_name: str) -> str:
+    """Generate description. """
+    github_server_url = os.environ.get('GITHUB_SERVER_URL', 'https://github.com')
+    github_repo = os.environ.get('GITHUB_REPOSITORY')
+    github_run_id = os.environ.get('GITHUB_RUN_ID')
+    return (
+        repo + ' - ' + workflow_name + ' failure\n'
+        + f'workflow failure URL: ${github_server_url}/${github_repo}/actions/runs/${github_run_id}\n'
     )
 
 
+def create_jira_issue(jira: str, project: str, repo: str, workflow_name: str, jira_type: str) -> any:
+    issue_dict = {
+        'project': {'key': project},
+        'summary': repo + " - " + workflow_name + " failure",
+        'description': get_description(repo, workflow_name),
+        'issuetype': {'name': jira_type},
+        'labels': [repo, workflow_name]
+    }
+    jira.issue_create(fields=issue_dict)
+
+
 def main():
-    repo = os.environ["GIT_REPO"]
-    jiraproject = os.environ["JIRA_PROJECT"]
-    jiratoken = os.environ["JIRA_TOKEN"]
-    jirahost = os.environ["JIRA_HOST"]
-    jirauser = os.environ["JIRA_USER"]
-    jira_type = os.environ["JIRA_TYPE"]
-    github_wf_name = os.environ["GITHUB_WORKFLOW_NAME"]
-    # adding suffix as some job types are key words in JQL and can't be used in query
-    github_wf_name_suffix = github_wf_name + "-job_type"
+    repo = os.environ.get('GITHUB_REPOSITORY').split('/')[1]
+    jira_project = os.environ.get('JIRA_PROJECT')
+    jira_token = os.environ.get('JIRA_TOKEN')
+    jira_host = os.environ.get('JIRA_HOST')
+    jira_user = os.environ.get('JIRA_USER')
+    jira_type = os.environ.get('JIRA_TYPE', 'Incident')
+    github_wf_name = os.environ.get('GITHUB_WORKFLOW')
+    github_wf_name_suffix = github_wf_name + '-job_type'
 
     # auth to jira endpoint using host, user and token
-    get_jira_auth = jira_auth(jirahost, jirauser, jiratoken)
+    get_jira_auth = jira_auth(jira_host, jira_user, jira_token)
     # checking if issue exist, creating issue if total count equals zero
-    get_issue_count = check_existing_issue(get_jira_auth, jiraproject, repo, github_wf_name_suffix)
+    get_issue_count = check_existing_issue(get_jira_auth, jira_project, repo, github_wf_name_suffix)
     if get_issue_count == 0:
-        create_jira_issue(get_jira_auth, jiraproject, repo, github_wf_name_suffix, jira_type)
+        create_jira_issue(get_jira_auth, jira_project, repo, github_wf_name_suffix, jira_type)
     else:
         print('not creating issue, already exist........')
 

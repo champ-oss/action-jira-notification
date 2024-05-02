986a3601e4dc7d41e544ad91450760165f6a9865
Phillip Langley <philliplangley@champmbplangley.lan>
2024-05-02 13:48:44 -0500

    adding feature to close job on success

--- action-jira-notification.py
+++ action-jira-notification.py
@@ -24,9 +24,9 @@ def check_existing_issue(jira: str, project: str, repo: str, workflow_name: str)
 
     data = jira.jql(jql)
     # checking json total value
-    get_total_issue_count = data["total"]
-
-    return get_total_issue_count
+    get_total_issue_count = data['total']
+    get_jira_issue_ids = data['issues']
+    return get_total_issue_count, get_jira_issue_ids
 
 
 def get_description(repo: str, workflow_name: str) -> str:
@@ -51,6 +51,10 @@ def create_jira_issue(jira: str, project: str, repo: str, workflow_name: str, ji
     jira.issue_create(fields=issue_dict)
 
 
+def close_jira_issue(jira: str, issue_id: str) -> any:
+    jira.issue_transition(issue_id, 'Done')
+
+
 def main():
     repo = os.environ.get('GITHUB_REPOSITORY').split('/')[1]
     jira_project = os.environ.get('JIRA_PROJECT')
@@ -61,12 +65,13 @@ def main():
     github_wf_name = os.environ.get('GITHUB_WORKFLOW')
     github_wf_name_suffix = github_wf_name + '-job_type'
 
-    # auth to jira endpoint using host, user and token
     get_jira_auth = jira_auth(jira_host, jira_user, jira_token)
-    # checking if issue exist, creating issue if total count equals zero
-    get_issue_count = check_existing_issue(get_jira_auth, jira_project, repo, github_wf_name_suffix)
-    if get_issue_count == 0:
+    get_issue_count, get_jira = check_existing_issue(get_jira_auth, jira_project, repo, github_wf_name_suffix)
+    if get_issue_count == 0 and os.environ.get('GITHUB_JOB_STATUS') == 'failure':
         create_jira_issue(get_jira_auth, jira_project, repo, github_wf_name_suffix, jira_type)
+    elif get_issue_count > 0 and os.environ.get('GITHUB_JOB_STATUS') == 'success':
+        for issue in get_jira:
+            close_jira_issue(get_jira_auth, issue['key'])
     else:
         print('not creating issue, already exist........')
 
